b49aee6bc6f047473d3138963545e929ac059235
pritam2510 <pritam.d95@gmail.com>
2023-11-06 15:01:07 +0530

    Online Methods Challenges

--- ExpresionStatementMethods/src/MethodChallenge1.java
+++ ExpresionStatementMethods/src/MethodChallenge1.java
@@ -6,20 +6,33 @@ public class MethodChallenge1 {
         displayHighScorePosition("Pritam", calculateHighScorePosition(100));
         displayHighScorePosition("Pritam", calculateHighScorePosition(25));
     }
-    public static void displayHighScorePosition(String playerName, int playerPosition ){
-        System.out.println(playerName + " managed to get into position " + playerPosition + " on the high score list");
+
+    public static void displayHighScorePosition(String playerName, int playerPosition) {
+        System.out.println(
+                playerName
+                        + " managed to get into position "
+                        + playerPosition
+                        + " on the high score list"
+        );
     }
 
-    public static int calculateHighScorePosition(int playerScore){
-        if(playerScore >= 1000){
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) {
             return 1;
-        }
-        else if(playerScore >= 500 && playerScore < 1000){
+        } else if (playerScore >= 500) {
             return 2;
-        }
-        else if(playerScore >= 100 && playerScore < 500){
+        } else if (playerScore >= 100) {
             return 3;
         }
         return 4;
+
+//        if (playerScore >= 1000) {
+//            return 1;
+//        } else if (playerScore >= 500 && playerScore < 1000) {
+//            return 2;
+//        } else if (playerScore >= 100 && playerScore < 500) {
+//            return 3;
+//        }
+//        return 4;
     }
 }
---
+++ ExpresionStatementMethods/src/OnlineMethodChakkenges.java
@@ -0,0 +1,96 @@
+public class OnlineMethodChakkenges {
+    public static void main(String[] args) {
+
+    }
+    // 1 CHALLENGE
+    public static void checkNumber (int num){
+        System.out.println(num > 0 ? "positive" : (num < 0 ? "negative" : "zero"));
+    }
+
+    // 2 CHALLENGE
+    public static long toMilesPerHour(double kmph) {
+        if(kmph < 0) return -1;
+        return (long)(Math.round(kmph / 1.609));
+    }
+
+    public static void printConversion (double kmph) {
+        if(kmph < 0) {System.out.println("Invalid Value"); return; }
+        System.out.println(kmph + " km/h = " + toMilesPerHour(kmph) + " mi/h");
+    }
+
+    // 3 CHALLENGE
+    public static void printMegaBytesAndKiloBytes(int kb) {
+        if(kb < 0){
+            System.out.println("Invalid Value");
+            return;
+        }
+        System.out.println(kb + " KB = " + (kb/1024) + " MB and " + (kb%1024) + " KB");
+    }
+
+    // 4 CHALLENGE
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        if(hourOfDay < 0 || hourOfDay > 23){
+            return false;
+        }
+        else if(hourOfDay < 8 || hourOfDay > 22){
+            return  barking;
+        }
+        return false;
+    }
+
+    // 5 CHALLENGE
+    public static boolean isLeapYear (int year){
+        if(year < 1 || year > 9999) return false;
+        else{
+            if(year % 4 == 0){
+                if(year % 100 == 0){
+                    return year % 400 == 0;
+                }
+                else{
+                    return true;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+    // MUCH SIMPLIFIED VERSION FROM COMMENTS
+    public static boolean isLeapYearSimplified(int year){
+        if (year<1 || year>9999){
+            return false;
+        }else{
+            return (year%4==0) && (year%100!=0) || year%400==0;
+        }
+    }
+
+    // 6 CHALLENGE
+    public static boolean areEqualByThreeDecimalPlaces (double first, double second){
+        return (((int)(first * 1000)) == ((int)(second * 1000)));
+    }
+
+    // WITHOUT CASTING VERSION FROM COMMENTS
+    public static boolean areEqualByThreeDecimalPlacesNoCasting(double value1, double value2){
+        double val1 = value1*1000;
+        double val2 = value2*1000;
+
+        double diff = val1 - val2;
+
+        return (diff > -1) && (diff < 1);
+    }
+
+    // 7 CHALLENGE
+    public static boolean hasEqualSum (int a, int b, int c) {
+        return c == a + b;
+    }
+
+    // 8 CHALLENGE
+    public static boolean isTeen (int num){
+        return num > 12 && num < 20;
+    }
+
+    public static boolean hasTeen  (int num1, int num2, int num3){
+        return isTeen(num1) || isTeen(num2) || isTeen(num3);
+    }
+}
